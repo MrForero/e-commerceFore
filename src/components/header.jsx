2ec752b20dab0113d0ceaa9cb493850017beb319
MrForero <foreroyatesantiago@gmail.com>
2025-05-06 15:40:44 -0500

    Header component

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export function Header(){
+    return <div className="header-container">
+                <h1>E-COMMERCE FORE</h1>
+                <h3>Quienes Somos</h3>
+                <h3>Contactenos</h3>
+                <button id="profile-button">
+                    <img src="./public/images/user-icon.png" alt="profile" />
+                </button>
+        </div>
+}
+
